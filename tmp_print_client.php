ce9e1139758a7ed852cffa9ea2722b3f4059d668
VINSMOKE-69 <tagam.abner77@gmail.com>
2026-03-08 03:01:03 +0800

    Remove generated secrets and temp scripts from repository

--- tmp_print_client.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require __DIR__ . '/bootstrap/app.php';
-
-use Illuminate\Support\Facades\DB;
-
-$client = DB::table('oauth_clients')->where('id', 3)->first();
-print_r($client);
